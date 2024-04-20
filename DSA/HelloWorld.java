1296274100811558a2118d4b386824070f893ee4
Shubham <sk37428@gmail.com>
2024-04-20 21:57:34 +0530

    Revering changes sorry

--- DSA/HelloWorld.java
+++ DSA/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String message = "Hello World";
-        System.out.println(message);
+        System.out.println("Hello World");
     }
 }
